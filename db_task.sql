c6be95686c22376958250394fa8363ed5c2ab5a6
Ghifari Octaverin <ghifariocta21@gmail.com>
2022-04-03 22:24:21 +0700

    initial commit

--- db_task.sql
+++ db_task.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 27, 2022 at 06:49 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- Generation Time: Apr 03, 2022 at 04:37 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.1.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -45,7 +44,12 @@ CREATE TABLE `tb_to_do` (
 INSERT INTO `tb_to_do` (`id`, `name_td`, `details_td`, `subject_td`, `priority_td`, `deadline_td`, `status_td`) VALUES
 (1, 'ERD', 'Bikin ERD', 'Basis Data', 'High', '2022-03-28', 'Sudah'),
 (6, 'TMD', 'Beresin TMD', 'Struktur Data', 'High', '2022-04-30', 'Belum'),
-(9, 'TP4', 'Kerjain TP4', 'DPBO', 'High', '2022-03-28', 'Belum');
+(9, 'TP4', 'Kerjain TP4', 'DPBO', 'High', '2022-03-28', 'Belum'),
+(16, 'LATIHAN7DPBO2022', 'Kerjain beb', 'DPBO', 'High', '2022-04-04', 'Belum'),
+(17, 'UTS', 'UTS SAY', 'Struktur Data', 'Low', '2022-04-05', 'Sudah'),
+(18, 'UTS DPW', 'kerjain utsnya say', 'Other', 'Medium', '2022-04-06', 'Belum'),
+(19, 'bikin db_vtuber', 'bikin db_vtuber isinya tentang vtuber', 'Basis Data', 'Low', '2022-04-30', 'Belum'),
+(20, 'UTS LORDDD', 'ya lord berikan ketabahan bagi hambamu ini', 'Other', 'Medium', '2022-04-14', 'Belum');
 
 --
 -- Indexes for dumped tables
@@ -65,7 +69,7 @@ ALTER TABLE `tb_to_do`
 -- AUTO_INCREMENT for table `tb_to_do`
 --
 ALTER TABLE `tb_to_do`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
